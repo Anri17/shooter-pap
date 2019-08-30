f584660fd91dd93dc2b7bbf97b801206f67470ed
Anri17 <andralmeida1999@gmail.com>
2019-08-30 22:40:57 +0100

    started implementation of homing bullet

---
+++ Assets/PointToTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointToTarget : MonoBehaviour
+{
+    public GameObject target;
+    // public float speed = 1.0f;
+    Vector3 direction;
+
+    void Start()
+    {
+        direction = target.transform.position - transform.position;
+        // Vector3 newDirection = Vector3.RotateTowards(transform.position, target.transform.position, 1.0f, 0.0f);
+        transform.rotation = Quaternion.LookRotation(direction);
+        // transform.position += direction * speed * Time.deltaTime;
+
+
+    }
+}
---
+++ Assets/ShootBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour
+{
+    public GameObject bullet;
+    public float fireInterval = 1.0f;
+
+    void Start()
+    {
+        InvokeRepeating("FireBullet", 0.0f, fireInterval);
+    }
+
+    void FireBullet()
+    {
+        Instantiate(bullet, transform.position, bullet.transform.rotation);
+    }
+}
