cf5c77c386cbbaead2a194aefd1986bd02c0a898
10162 <10162@formacao.itap-net.org>
2019-10-14 11:26:49 +0100

    added power item collectible texture

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data
+{
+    public int score = 0;
+    public float powerLevel = 0.0f;
+    public int lives = 3;
+}
