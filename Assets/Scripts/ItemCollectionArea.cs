3c8a505b6dd0d505f0553384654e9313841591e3
10162 <10162@formacao.itap-net.org>
2019-10-15 17:29:01 +0100

    added start of the item collection area

---
+++ Assets/Scripts/ItemCollectionArea.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollectionArea : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player is in the item collection area.");
+        }        
+    }
+}
