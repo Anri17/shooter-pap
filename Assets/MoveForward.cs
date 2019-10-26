16144df9082c4dadb9ee643e90d39a01c6a447e6
Anri17 <andralmeida1999@gmail.com>
2019-10-26 03:09:24 +0100

    added moving waving enemy

---
+++ Assets/MoveForward.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
