bcc78224be2cd4cad40b3ada22ecd4cc0a433773
Anri17 <andralmeida1999@gmail.com>
2019-10-27 00:01:15 +0100

    Consolidated fire bullet scripts into one

---
+++ Assets/MoveObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public int horizontalDirection = 1;
+    public int verticalDirection = 1;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(horizontalDirection, verticalDirection, 0);
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/FireBullet.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject targetObject;
+    public Vector3 targetCoords = new Vector3(0, -1, 0);
+    public bool fireRepeat = false;
+    public int fireBulletCount = 0;
+    public float fireRate;
+    public float offset = -90;
+
+    int bulletCounter = 0;
+    Quaternion direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // fire the bullet
+        InvokeRepeating("Fire", 0.0f, fireRate / 100);
+    }
+
+    void Fire()
+    {
+        // define the direction of the bullet
+        if (targetObject != null)   // set the direction if there's a game object to target
+        {
+            Vector3 difference = targetObject.transform.position - transform.position;
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            direction = Quaternion.Euler(0f, 0f, rotZ + offset);
+        }
+        else // otherwise set the direction based on coordinates given
+        {
+            Vector3 target = transform.position + targetCoords;
+            Vector3 difference = target - transform.position;
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            direction = Quaternion.Euler(0f, 0f, rotZ + offset);
+        }
+
+        if (fireRepeat) // fire bullets indefinitely, or until the object is destroyed
+        {
+            Instantiate(bullet, transform.position, direction);
+        }
+        else // fire a determined ammount of bullet
+        {
+            Instantiate(bullet, transform.position, direction);
+            if (++bulletCounter >= fireBulletCount)
+            {
+                CancelInvoke("Fire");
+            }
+        }
+    }
+}
