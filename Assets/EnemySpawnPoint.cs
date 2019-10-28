07962d898be6f0c28f3f87062b33706ce3c2a29b
10162 <10162@formacao.itap-net.org>
2019-10-28 10:57:08 +0000

    added spawn point

---
+++ Assets/EnemySpawnPoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnPoint : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnRate = 100.0f;
+    public bool spawnDefinedCount = false;
+    public int spawnCount;
+
+    private int spawnCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (enemy != null)
+            InvokeRepeating("SpawnEnemy", 0.0f, spawnRate / 100);
+    }
+
+    void SpawnEnemy()
+    {
+        if (!spawnDefinedCount)
+        {
+            Instantiate(enemy, gameObject.transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(enemy, gameObject.transform.position, Quaternion.identity);
+            if (++spawnCounter >= spawnCount)
+            {
+                CancelInvoke("SpawnEnemy");
+            }
+        }
+    }
+}
