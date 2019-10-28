3215e898287e9e9723eaea2029408f8105eba96e
10162 <10162@formacao.itap-net.org>
2019-10-28 10:35:12 +0000

    player destroyed on respawn if it exists

--- Assets/Scripts/PlayMenuHandler.cs
+++ Assets/Scripts/PlayMenuHandler.cs
@@ -8,6 +8,11 @@ public class PlayMenuHandler : MonoBehaviour
 
     public void RespawnPlayer()
     {
+        // detect if a player already exists in the play area
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Player"));
+        }
         // spawn player
         Instantiate(player, new Vector3(-6.0f, -7.0f, 0.0f), player.transform.rotation);
         // clear enemy bullets
