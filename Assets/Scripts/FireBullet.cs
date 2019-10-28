205a639255335a056776ba0504a2339d5cece81d
10162 <10162@formacao.itap-net.org>
2019-10-28 10:24:35 +0000

    added target detection for targeted bullets

--- Assets/Scripts/FireBullet.cs
+++ Assets/Scripts/FireBullet.cs
@@ -14,30 +14,46 @@ public class FireBullet : MonoBehaviour
 
     int bulletCounter = 0;
     Quaternion direction;
+    string targetObjectTag = "";
 
     // Start is called before the first frame update
     void Start()
     {
+        // get and store target object tag
+        if (targetObject != null)
+        {
+            targetObjectTag = targetObject.tag;
+        }
+
         // fire the bullet
         InvokeRepeating("Fire", 0.0f, fireRate / 100);
     }
 
-    void Fire()
+    Quaternion GetDirection() // define the direction of the bullet
     {
-        // define the direction of the bullet
-        if (targetObject != null)   // set the direction if there's a game object to target
+        if (!targetObjectTag.Equals("")) // define object with tag extracted
+        {
+            targetObject = GameObject.FindGameObjectWithTag(targetObjectTag);
+        }
+        
+        if (targetObject != null) // set the direction if there's a game object to target
         {
             Vector3 difference = targetObject.transform.position - transform.position;
             float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-            direction = Quaternion.Euler(0f, 0f, rotZ + offset);
+            return Quaternion.Euler(0f, 0f, rotZ + offset);
         }
         else // otherwise set the direction based on coordinates given
         {
             Vector3 target = transform.position + targetCoords;
             Vector3 difference = target - transform.position;
             float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-            direction = Quaternion.Euler(0f, 0f, rotZ + offset);
+            return Quaternion.Euler(0f, 0f, rotZ + offset);
         }
+    }
+
+    void Fire()
+    {
+        direction = GetDirection(); // assign direction of the bullet
 
         if (fireRepeat) // fire bullets indefinitely, or until the object is destroyed
         {
