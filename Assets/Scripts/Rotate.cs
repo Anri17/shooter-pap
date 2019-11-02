4a1d68a8987629534d62d5df3c0478e57928f7c2
Anri17 <andralmeida1999@gmail.com>
2019-11-02 17:58:46 +0000

    added self rotating bullet

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float angle = 0;
+    public float rotationSpeed = 8f;
+
+    float rotationAngle = 0;
+
+    void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), Mathf.Cos(angle * Mathf.Deg2Rad), 0);
+        Debug.DrawRay(transform.position, direction * 3, Color.green);
+        
+        // rotate object
+        transform.rotation = Quaternion.Euler(0, 0, -rotationAngle);
+        rotationAngle += rotationSpeed;
+    }
+}
