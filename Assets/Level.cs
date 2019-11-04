fda9fd16860e1b4f526e5897bc313f9be0f23aad
10162 <10162@formacao.itap-net.org>
2019-11-04 13:18:42 +0000

    start code of enemy movement

--- Assets/Level.cs
+++ Assets/Level.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 1.0f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(0, -1f, 0) * speed * Time.deltaTime;
     }
+
 }
