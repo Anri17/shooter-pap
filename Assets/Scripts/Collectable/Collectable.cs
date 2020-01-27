87cbe8f0f4ba2e642f939508ac9a22f73b832da7
10162 <10162@formacao.itap-net.org>
2020-01-27 09:57:14 +0000

    Attract items to player when in ItemCollectionArea

--- Assets/Scripts/Collectable/Collectable.cs
+++ Assets/Scripts/Collectable/Collectable.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    public void Move(GameObject target)
+    public void Move(Transform target, float moveSpeed)
     {
-        transform.Translate(target.transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/ItemCollectionArea.cs
+++ Assets/Scripts/ItemCollectionArea.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ItemCollectionArea : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 90f;
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag.Equals("Player"))
         {
+            GameObject player = collision.gameObject;
             GameObject[] powerCollectables = GameObject.FindGameObjectsWithTag("PowerCollectable");
             GameObject[] bigPowerCollectables = GameObject.FindGameObjectsWithTag("BigPowerCollectable");
             GameObject[] scoreCollectables = GameObject.FindGameObjectsWithTag("ScoreCollectable");
@@ -15,17 +18,17 @@ public class ItemCollectionArea : MonoBehaviour
             for (int i = 0; i < powerCollectables.Length; i++)
             {
                 Collectable powerCollectable = powerCollectables[i].GetComponent<Collectable>();
-                powerCollectable.Move(collision.gameObject);
+                powerCollectable.Move(player.transform, moveSpeed);
             }
             for (int i = 0; i < bigPowerCollectables.Length; i++)
             {
                 Collectable bigPowerCollectable = bigPowerCollectables[i].GetComponent<Collectable>();
-                bigPowerCollectable.Move(collision.gameObject);
+                bigPowerCollectable.Move(player.transform, moveSpeed);
             }
             for (int i = 0; i < scoreCollectables.Length; i++)
             {
                 Collectable scoreCollectable = scoreCollectables[i].GetComponent<Collectable>();
-                scoreCollectable.Move(collision.gameObject);
+                scoreCollectable.Move(player.transform, moveSpeed);
             }
         }        
     }
