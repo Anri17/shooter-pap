ae40a8e253340c839431378bcce7a129c9e246bf
Anri17 <andralmeida1999@gmail.com>
2020-02-18 21:58:47 +0000

    Cleanup project

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -4,15 +4,9 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    public float backgroundImageScrollSpeed = 1.0f;
-    public GameObject backgroundImage;
     public GameObject playerSpawnPoint;
-    public GameObject boss;
     public AudioClip stageMusicTheme;
     public AudioClip bossMusicTheme;
-    public GameObject[] waves;
-
-    bool moveBackground = true;
 
     Player player;
     GameManager gameManager;
@@ -22,10 +16,6 @@ public class LevelManager : MonoBehaviour
     {
         gameManager = GameManager.Instance;
         musicPlayer = AudioPlayer.Instance;
-        foreach (GameObject wave in waves)
-        {
-            wave.SetActive(false);
-        }
     }
 
     void Start()
@@ -34,19 +24,6 @@ public class LevelManager : MonoBehaviour
         musicPlayer.PlayMusic(stageMusicTheme);
         SpawnPlayer(playerSpawnPoint.transform);
         player = gameManager.spawnedPlayer.GetComponent<Player>();
-    }
-        
-    void Update()
-    {
-        if (moveBackground)
-        {
-            MoveBackground(backgroundImageScrollSpeed);
-        }
-    }
-
-    private void MoveBackground(float speed)
-    {
-        backgroundImage.transform.position += new Vector3(0, -1f, 0) * speed * Time.deltaTime;
     }
 
     public void SpawnPlayer(Transform position)
@@ -59,21 +36,6 @@ public class LevelManager : MonoBehaviour
         {
             player.SpawnPlayer(playerSpawnPoint.transform.position);
         }
-    }
-
-    public void SpawnPowerItem()
-    {
-        Instantiate(gameManager.powerItem, new Vector3(-2.56f, 5.51f, 0), gameManager.powerItem.transform.rotation);
-    }
-
-    public void SpawnBigPowerItem()
-    {
-        Instantiate(gameManager.bigPowerItem, new Vector3(-2.56f, 5.51f, 0), gameManager.bigPowerItem.transform.rotation);
-    }
-
-    public void SpawnScoreItem()
-    {
-        Instantiate(gameManager.scoreItem, new Vector3(-2.56f, 5.51f, 0), gameManager.scoreItem.transform.rotation);
     }
 
     public static void ClearBullets()
