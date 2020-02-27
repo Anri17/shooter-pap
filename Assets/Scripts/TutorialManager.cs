56d658fad9fa19d2f535015d7b7ba620fad79327
10162 <10162@formacao.itap-net.org>
2020-02-27 09:50:03 +0000

    Add new tutorial message

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -9,6 +9,7 @@ public class TutorialManager : MonoBehaviour
     private int popUpIndex;
     public GameObject testWave;
     public bool canContinue = false;
+    private bool resetContinue = true;
 
     private void Update()
     {
@@ -22,6 +23,12 @@ public class TutorialManager : MonoBehaviour
             {
                 popUps[i].SetActive(false);
             }
+        }
+
+        if (resetContinue)
+        {
+            canContinue = false;
+            resetContinue = false;
         }
 
         if (popUpIndex == 0)
@@ -40,12 +47,21 @@ public class TutorialManager : MonoBehaviour
             }
             if (Input.GetKey(KeyCode.Return) && canContinue)
             {
+                resetContinue = true;
                 popUpIndex++;
             }
         }
         else if (popUpIndex == 2)
         {
-
+            if (Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)))
+            {
+                canContinue = true;
+            }
+            if (Input.GetKey(KeyCode.Return) && canContinue)
+            {
+                resetContinue = true;
+                popUpIndex++;
+            }
         }
     }
 }
