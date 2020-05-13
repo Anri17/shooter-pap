84a0325138af410d5dbb3b3f65f053e58a4330d6
Anri17 <andralmeida1999@gmail.com>
2020-05-13 20:25:27 +0100

    Replace music files

--- Assets/Scripts/Dialogue/DialogueConversation.cs
+++ Assets/Scripts/Dialogue/DialogueConversation.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Dialogue Conversation", menuName = "Dialogue/DialogueConversation")]
-
 public class DialogueConversation : ScriptableObject
 {
     public DialogueMessage[] DialogueMessages;
