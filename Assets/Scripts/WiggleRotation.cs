a0f80a62372b1329e440ac5e4787f30aa192e189
Anri17 <andralmeida1999@gmail.com>
2020-05-19 02:01:44 +0100

    Prepare stage 2

---
+++ Assets/Scripts/WiggleRotation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiggleRotation : MonoBehaviour
+{
+    public float angle = 0;
+    public float rotationSpeed = 2f;
+
+    float rotationAngle = 0;
+
+    public float angleThreshhold = 60;
+
+    bool negative = false;
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, -rotationAngle);
+
+
+        if (negative)
+        {
+            rotationAngle += -rotationSpeed;
+            if (rotationAngle < -angleThreshhold)
+            {
+                negative = false;
+            }
+        }
+        else if (!negative)
+        {
+            rotationAngle += rotationSpeed;
+            if (rotationAngle > angleThreshhold)
+            {
+                negative = true;
+            }
+        }
+
+        
+        
+        
+
+        
+        Debug.Log(transform.rotation.z);
+    }
+
+}
