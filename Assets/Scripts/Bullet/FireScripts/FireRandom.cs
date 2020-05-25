03dee18132da77e976689dc51f4fc660feeef63e
Anri17 <andralmeida1999@gmail.com>
2020-05-25 20:23:30 +0100

    Finish Stage 2 Waves

---
+++ Assets/Scripts/Bullet/FireScripts/FireRandom.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireRandom : FireBullet
+{
+    [Header("Fire Random")]
+    [SerializeField] float startDelay = 1f;
+    [SerializeField] float fireDelay;
+    [SerializeField] int fireCount;
+
+    Coroutine fireCoroutine;
+
+    void Fire()
+    {
+        bulletGameObject = Instantiate(_bullet, transform.position, Quaternion.identity);
+        bulletScript = bulletGameObject.GetComponent<Bullet>();
+
+        bulletScript.Speed = _speed;
+        bulletScript.Angle = _aimOffset + GetRandomAngle();
+    }
+
+    IEnumerator RepeatFire(float startDelay, float fireDelay, int fireCount)
+    {
+        yield return new WaitForSeconds(startDelay);
+
+        if (fireCount > 0)
+        {
+            int initialBullet = 0;
+            while (initialBullet < fireCount)
+            {
+                Fire();
+                yield return new WaitForSeconds(fireDelay);
+                initialBullet++;
+            }
+            StopCoroutine(fireCoroutine);
+        }
+        else
+        {
+            while (true)
+            {
+                Fire();
+                yield return new WaitForSeconds(fireDelay);
+            }
+        }
+    }
+
+    bool stay = false;
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayArea") && stay == false)
+        {
+            fireCoroutine = StartCoroutine(RepeatFire(startDelay, fireDelay, fireCount));
+            stay = true;
+            Debug.Log("I'm in the play field");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayArea"))
+        {
+            StopCoroutine(fireCoroutine);
+        }
+    }
+
+    float GetRandomAngle()
+    {
+        return Random.Range(-180, 180);
+    }
+}
---
+++ Assets/Scripts/Movement/SpellMovement/MovementLinear.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MovementDirection
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+public class MovementLinear : SpellMovement
+{
+    [SerializeField] MovementDirection movementDirection;
+    [SerializeField] float firstMovementSpeed;
+    [SerializeField] float movementDuration;
+    [SerializeField] float secondMovementSpeed;
+
+    float speedModifier;
+
+    public override IEnumerator MoveCoroutine()
+    {
+        StartCoroutine(ChangeSpeedCoroutine(movementDuration));
+        speedModifier = firstMovementSpeed;
+
+        switch (movementDirection)
+        {
+            case MovementDirection.Up:
+                while (true)
+                {
+                    mainTransform.Translate(Vector3.up * (speedModifier / 10) * Time.deltaTime);
+                    yield return null;
+                }
+                break;
+            case MovementDirection.Down:
+                while (true)
+                {
+                    mainTransform.Translate(Vector3.down * (speedModifier / 10) * Time.deltaTime);
+                    yield return null;
+                }
+                break;
+            case MovementDirection.Left:
+                while (true)
+                {
+                    mainTransform.Translate(Vector3.left * (speedModifier / 10) * Time.deltaTime);
+                    yield return null;
+                }
+                break;
+            case MovementDirection.Right:
+                while (true)
+                {
+                    mainTransform.Translate(Vector3.right * (speedModifier / 10) * Time.deltaTime);
+                    yield return null;
+                }
+                break;
+            default:
+                mainTransform.Translate(Vector3.down * (speedModifier / 10) * Time.deltaTime);
+                yield return null;
+                break;
+        }
+    }
+
+    IEnumerator ChangeSpeedCoroutine(float movementduration)
+    {
+        yield return new WaitForSeconds(movementduration);
+        speedModifier = secondMovementSpeed;
+    }
+
+}
